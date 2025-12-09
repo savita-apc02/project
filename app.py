1871b561480e4325f0df90666cf6aaaffc131ef8
savita-apc02 <savita.apc02@gmail.com>
2025-12-09 11:37:50 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,34 +1,28 @@
 from flask import Flask, request, render_template
+from moviepy.editor import VideoFileClip
 import os
-import subprocess
 
 app = Flask(__name__)
 
-# uploads folder create
 if not os.path.exists("uploads"):
     os.makedirs("uploads")
 
 @app.route("/", methods=["GET", "POST"])
 def home():
     if request.method == "POST":
-
         file = request.files["file"]
         filepath = os.path.join("uploads", file.filename)
         file.save(filepath)
 
-        # output file name
+        clip = VideoFileClip(filepath)
         output = filepath.rsplit(".", 1)[0] + ".mp3"
-
-        # ffmpeg convert command
-        command = f"ffmpeg -i \"{filepath}\" \"{output}\""
-
-        # run ffmpeg
-        subprocess.run(command, shell=True)
+        clip.audio.write_audiofile(output)
 
         return f"Converted Successfully! Saved as: {output}"
 
     return render_template("index.html")
 
-
 if __name__ == "__main__":
     app.run()
+
+
